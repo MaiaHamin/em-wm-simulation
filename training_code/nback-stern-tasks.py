00cf6e67a70df186658b27fb293ed14a0418688e
Maia Hamin <maia@Maias-MacBook-Pro.local>
2021-01-18 08:34:15 -0500

    Putting in new names

---
+++ training_code/nback-stern-tasks.py
@@ -0,0 +1,220 @@
+import numpy as np
+import torch as tr
+
+def single_sternberg_comparison(stimset,cdrift,setsize,
+  pr_match,pr_stim_lure,pr_context_lure):
+  # task params
+  stimset_size,sdim = stimset.shape
+
+  # set current stim and context
+  stim_t_idx = np.random.randint(stimset_size)
+  stim_t = stimset[stim_t_idx,:]
+  context_t_idx = setsize * 2 + 1 # the current trace is always the test probe from the second list
+  context_t = cdrift[context_t_idx]
+
+  ## define positive and negative samples
+  stim_pos = stim_t
+  context_pos_idx = context_t_idx - np.random.randint(0, setsize)
+  context_pos = cdrift[context_pos_idx]
+  # negative context, different trial
+  context_neg_idx = context_t_idx - np.random.randint(setsize + 1, setsize * 2 + 1)
+  context_neg = cdrift[context_neg_idx]
+  # stim
+  stim_neg_idx = np.random.choice(np.setdiff1d(range(stimset_size), stim_t_idx))
+  stim_neg = stimset[stim_neg_idx]
+
+  # trial type
+  ttype_randn = np.random.random()
+
+  # both match
+  if ttype_randn<pr_match:
+    # positive trial
+    stim_m  = stim_pos
+    context_m  = context_pos
+    ytarget = tr.LongTensor([1])
+    ttype_idx = 0
+  # slure: stim match (context no match)
+  elif ttype_randn<pr_match+pr_stim_lure:
+    # stim lure
+    stim_m = stim_pos
+    context_m = context_neg
+    ytarget = tr.LongTensor([0])
+    ttype_idx = 2
+  # clure: context match (stim no match)
+  elif ttype_randn<pr_match+pr_stim_lure+pr_context_lure:
+    # context lure
+    stim_m = stim_neg
+    context_m = context_pos
+    ytarget = tr.LongTensor([0])
+    ttype_idx = 1
+  # neither match
+  else:
+    # negative trial
+    stim_m = stim_neg
+    context_m = context_neg
+    ytarget = tr.LongTensor([0])
+    ttype_idx = 3
+  return stim_t,context_t,stim_m,context_m,ytarget,ttype_idx
+
+def single_nback_comparison(stimset,cdrift,setsize,
+  pr_match,pr_stim_lure,pr_context_lure):
+  """ 
+  Returns a pair of a stimulus-context traces representing a currently-active stimulus and context & a single trace
+  retrieved from memory. Generates this pair based on the four comparison types laid out in the paper:
+    both-match, stim lure, context lure, neither match.
+  output is a 4-tuple (s_t,c_t,s_r,c_r), as well as ytarget and ttype_code which encodes the trial type for error reporting.
+  """
+  ntokens,sdim = stimset.shape
+  min_context_t = setsize
+
+  # set current stim and context
+  stim_t_idx = np.random.randint(0,ntokens)
+  context_t_idx = np.random.randint(min_context_t, ntokens)
+  stim_t = stimset[stim_t_idx]
+  context_t = cdrift[context_t_idx]
+
+  ttype_randn = np.random.random()  # randomly-selected trial type
+  ttype_code = -1 # code used to record trial type for analysis
+
+  if ttype_randn < pr_match:
+    stim_m, context_m = nback_both_match(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+    ytarget = tr.LongTensor([1])
+    ttype_code = 0
+
+  elif ttype_randn < (pr_match + pr_context_lure):
+    stim_m, context_m = nback_ctxt_lure(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+    ytarget = tr.LongTensor([0])
+    ttype_code = 1
+
+  elif ttype_randn < (pr_match + pr_context_lure + pr_stim_lure):
+    stim_m, context_m = nback_stim_lure(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+    ytarget = tr.LongTensor([0])
+    ttype_code = 2
+
+  else:
+    stim_m, context_m = nback_neither_match(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+    ytarget = tr.LongTensor([0])
+    ttype_code = 3
+
+  return stim_t,stim_m,context_t,context_m,ytarget, ttype_code
+
+def sample_nback_retrieval_set_for_trial_sequence(stimset, cdrift, setsize,
+                                          pr_match, pr_stim_lure, pr_context_lure):
+    """
+    Simulates the set of traces in EM corresponding to a possible n-back sequence for a particular trial type,
+    returning an list of tuples (with the current trial is at index 0)
+    of stimulus and context information, as well as the true answer ytarget and a code for the trial type.
+    """
+    n_traces = 10
+    trace_seq = [None] * n_traces
+    ntokens,sdim = stimset.shape
+    min_ctxt_t = ntokens - setsize
+
+    ## current stim and context
+    stim_t_idx = np.random.randint(0,ntokens)
+    context_t_idx = np.random.randint(min_ctxt_t)
+    stim_t = stimset[stim_t_idx]
+    context_t = cdrift[context_t_idx]
+    trace_seq[0] = (stim_t, context_t) # the 0th index is the current stim, context
+
+    ttype_rand_n = np.random.random() # random number for determining the trial type
+    ttype_code = -1  # code for the trial type that will be returned - 0 if a control, and 1 if a lure
+
+    # control target
+    if ttype_rand_n < pr_match:
+        # One trace in the n-back position with a matching stimulus
+        trace_seq[setsize] = nback_both_match(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+        # All other traces in the sequence are from non-nback positions and have non-matching stimuli
+        for i in range(1, n_traces):
+          if i != setsize:
+              trace_seq[i] = nback_neither_match(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+        ytarget = tr.LongTensor([1])
+        ttype_code = 0
+
+    # control foil
+    elif ttype_rand_n < (pr_match + pr_context_lure):
+        # One trace in the n-back position with a non-matching stimulus
+        trace_seq[setsize] = nback_ctxt_lure(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+        # All other traces in the sequence have non-matching stimuli
+        for i in range(1, n_traces):
+            if i != setsize:
+                trace_seq[i] = nback_neither_match(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+        ytarget = tr.LongTensor([0])
+        ttype_code = 0
+
+    # lure target
+    elif ttype_rand_n < (pr_match + pr_context_lure + pr_stim_lure):
+        # One trace in the n-back position with a matching stimulus
+        trace_seq[setsize] = nback_both_match(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+        # One trace in the non n-back position has a non-matching stimulus
+        trace_seq[setsize - 1] = nback_stim_lure(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+        # All other traces have non-matching stimuli
+        for i in range(1, n_traces):
+            if i != setsize and i != setsize - 1:
+                trace_seq[i] = nback_neither_match(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+        ytarget = tr.LongTensor([1])
+        ttype_code = 1
+
+    # lure foil
+    else:
+      # The trace in the n-back position has a non-matching stimulus
+      trace_seq[1] = nback_ctxt_lure(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+      # The trace one-after the n-back position (the lure position) has a matching stimulus
+      trace_seq[2] = nback_stim_lure(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+      # All other traces in the sequence other than the n-back position have non-matching stimuli
+      for i in range(3, n_traces):
+        trace_seq[i] = nback_neither_match(stimset, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+      ytarget = tr.LongTensor([0])
+      ttype_code = 1
+
+    return trace_seq, ytarget, ttype_code
+
+# return a both match trace -- the stimuli match, and the context is the n-back context
+def nback_both_match(stim_set, cdrift, stim_t_idx, context_t_idx, setsize, ntokens):
+    stim_m = stim_set[stim_t_idx]
+    context_m = cdrift[context_t_idx + setsize]
+    return (stim_m, context_m)
+
+# return a stim lure trace -- the stimuli match, but the context is not the n-back context
+def nback_stim_lure(stim_set, cdrift, stim_t_idx, context_t_idx, setsize, ntokens):
+    stim_m = stim_set[stim_t_idx]
+    nback_context_idx = context_t_idx + setsize
+    rlo = context_t_idx + 1
+    rhi = min(nback_context_idx + 4, ntokens)
+    idx_context_m = np.random.choice(np.setdiff1d(range(rlo, rhi), nback_context_idx))
+    context_m = cdrift[idx_context_m]
+    return (stim_m, context_m)
+
+# return a context lure trace -- the stimuli don't match, but the context is the n-back context
+def nback_ctxt_lure(stim_set, cdrift, stim_t_idx, context_t_idx, setsize, ntokens):
+    idx_stim_m = np.random.choice(np.setdiff1d(range(ntokens), stim_t_idx))
+    stim_m = stim_set[idx_stim_m]
+    context_m = cdrift[context_t_idx + setsize]
+    return (stim_m, context_m)
+
+# return a neither match trace -- the stimuli don't match, and the context is not the n-back context.
+# optionally, for the EM simulations, this can probabilistically return a matching stimulus and a long-past context
+# (s.t. the trace isn't a proper lure, but simulates the repeating-stimuli dynamics of the task).
+def nback_neither_match(stim_set, cdrift, stim_t_idx, context_t_idx, setsize, ntokens, pr_prewindow_match = 0.1):
+    if np.random.uniform() > pr_prewindow_match or ntokens - context_t_idx < 6:
+        idx_stim_m = np.random.choice(np.setdiff1d(range(ntokens), stim_t_idx))
+        nback_context_idx = context_t_idx + setsize
+        rlo = context_t_idx + 1
+        rhi = min(nback_context_idx + 4, ntokens)
+        idx_context_m = np.random.choice(np.setdiff1d(range(rlo, rhi), nback_context_idx))
+        stim_m = stim_set[idx_stim_m]
+        context_m = cdrift[idx_context_m]
+        return stim_m, context_m
+    else:
+        return nback_distant_slure(stim_set, cdrift, stim_t_idx, context_t_idx, setsize, ntokens)
+
+# A trial type where the stimulus DOES match, but the context is so far from the target context that it isn't a lure
+def nback_distant_slure(stim_set, cdrift, stim_t_idx, context_t_idx, setsize, ntokens):
+    idx_stim_m = np.random.choice(np.setdiff1d(range(ntokens), stim_t_idx))
+    nback_context_idx = context_t_idx + setsize
+    rlo = nback_context_idx + 1
+    rhi = min(nback_context_idx + 10, ntokens)
+    idx_context_m = np.random.choice(range(rlo, rhi))
+    stim_m = stim_set[idx_stim_m]
+    context_m = cdrift[idx_context_m]
+    return stim_m, context_m
---
+++ training_code/train-wm-model.py
@@ -0,0 +1,288 @@
+import torch as tr
+import numpy as np
+from matplotlib import pyplot as plt
+from drift_fns import fast_n_sphere
+from generate_stimset import gen_stims
+from sampling_dprime import sampling_dprime
+
+class FFWM(tr.nn.Module):
+  """ Model used for Sternberg and N-back """
+  def __init__(self,indim,hiddim,outdim=2,bias=False,add_noise=False):
+    super().__init__()
+    self.indim = indim
+    self.hiddim = hiddim
+    self.add_noise = add_noise
+    self.hid1_layer = tr.nn.Linear(indim,indim,bias=bias)
+    self.hid2_layer = tr.nn.Linear(indim,hiddim,bias=bias)
+    self.out_layer = tr.nn.Linear(hiddim,outdim,bias=bias)
+    self.drop2 = tr.nn.Dropout(p=0.05, inplace=False)
+    bias_dim = indim
+    max_num_bias_modes = 10
+    self.embed_bias = tr.nn.Embedding(max_num_bias_modes,bias_dim)
+    return None
+
+  def forward(self,inputL,control_bias_int=0):
+    """ inputL is list of tensors """
+    hid1_in = tr.cat(inputL,-1)
+    hid1_act = self.hid1_layer(hid1_in).relu()
+    control_bias = self.embed_bias(tr.tensor(control_bias_int))
+    hid2_in = hid1_act + control_bias
+    if self.add_noise:
+      hid2_in = hid2_in + (0.1 ** 0.5) * tr.randn(hid2_in.shape)
+    hid2_in = self.drop2(hid2_in)
+    hid2_act = self.hid2_layer(hid2_in).relu()
+    yhat_t = self.out_layer(hid2_act)
+    return yhat_t
+
+def run_model_for_epochs(net, taskL, cfun, training, neps_per_task, n_context_steps, stim_v, verb=True):
+  if training:
+    net.train()
+  else:
+    net.eval()
+  lossop = tr.nn.CrossEntropyLoss()
+  optiop = tr.optim.Adam(net.parameters(), lr=0.001)
+  score = -np.ones([len(taskL), neps_per_task])
+  ttype = -np.ones([len(taskL), neps_per_task])
+  for ep in range(neps_per_task):
+    stimset = gen_stims(stim_v)
+    if verb and ep % (neps_per_task / 5) == 0:
+      print(ep / neps_per_task)
+    # resample stim and context on each ep
+    cdrift = cfun(n_steps=n_context_steps, idx=ep)
+    cdrift = tr.Tensor(cdrift)
+    # interleave train on every task per epoch
+    for task_idx,(control_int,sample_trial_fn,setsize) in enumerate(taskL): 
+      # use the input function to generate a trial sample
+      out = sample_trial_fn(stimset,cdrift,setsize)
+      stim_t,stim_m,context_t,context_m,ytarget, ttype_idx = out
+      # forward prop
+      inputL = [stim_t,stim_m,context_t,context_m]
+      yhat = net(inputL,control_bias_int=control_int)
+      # eval
+      score[task_idx, ep] = maxsoftmax(yhat)==ytarget
+      ttype[task_idx, ep] = ttype_idx
+      # backprop
+      if training:
+        eploss = lossop(yhat.unsqueeze(0), ytarget)
+        optiop.zero_grad()
+        eploss.backward(retain_graph=True)
+        optiop.step()
+  return score, ttype
+
+maxsoftmax = lambda x: tr.argmax(tr.softmax(x,-1),-1).squeeze()
+
+def train_model_intervals(net,taskL,long_cfun,cfun,training,neps_per_task,
+                          n_context_steps,model_name,stim_v,save_intermediate=False, verb=True):
+  if training:
+    net.train()
+  else:
+    net.eval()
+  lossop = tr.nn.CrossEntropyLoss()
+  optiop = tr.optim.Adam(net.parameters(), lr=0.001)
+  score = -np.ones([len(taskL), neps_per_task])
+  ttype = -np.ones([len(taskL), neps_per_task])
+  for ep in range(neps_per_task):
+    if verb and ep % (neps_per_task / 5) == 0:
+      print(ep / neps_per_task)
+    stimset = gen_stims(stim_v)  # tr.load(stimset_name + ".tr")
+    # resample stim and context on each ep
+    cdrift = long_cfun(n_steps=n_context_steps, idx=ep)
+    cdrift = tr.Tensor(cdrift)
+    # interleave train on every task per epoch
+    for task_idx,(control_int,sample_trial_fn,setsize) in enumerate(taskL):
+      # generate trial sample
+      out = sample_trial_fn(stimset,cdrift,setsize)
+      stim_t,stim_m,context_t,context_m,ytarget, ttype_idx = out
+      # forward prop
+      inputL = [stim_t,stim_m,context_t,context_m]
+      yhat = net(inputL,control_bias_int=control_int)
+      # eval
+      score[task_idx, ep] = maxsoftmax(yhat)==ytarget
+
+      ttype[task_idx, ep] = ttype_idx
+      # backprop
+      if training:
+        eploss = lossop(yhat.unsqueeze(0), ytarget)
+        optiop.zero_grad()
+        eploss.backward(retain_graph=True)
+        optiop.step()
+    if save_intermediate:
+      if ep % 25000 == 0:
+        sampling_dprime(net, model_name, cfun, mean, var, beta, dim, fname='../Figures/interval_training/' + str(ep) + "_")
+        np.save('interval_nets/trsc-' + model_name + str(ep), score)
+        tr.save(net.state_dict(), 'interval_nets/trsc-' + model_name + str(ep) + ".pt")
+
+  return score, ttype
+
+# vars
+def run_all(seed, ctxt_fn, mean, var, beta, dim, neps):
+  np.random.seed(seed)
+  tr.random.manual_seed(seed)
+
+  # task params
+  task = "nback" # "stern"
+  stim_ntokens = 20
+  num_context_steps = 20
+
+  ## net params
+  sdim = stim_ntokens
+  indim = 2 * (dim + sdim)
+  hiddim = dim * 4
+
+  # train / ev params
+  stimset_name = "stimset-v=075"
+  pr_match, pr_slure, pr_clure = 0.4, 0.2, 0.2
+  # model_name = 'ffwm-%s-ss49strictlure-tr_noise_%i-seed_%i'%(task,noise_tr*10,seed)
+  model_name = 'ffwm-ttype%s_probs%i%i%i--neps%i-seed%i' % (
+  task, pr_match * 10, pr_slure * 10, pr_clure * 10, neps, seed)
+  long_context, fn_name = ctxt_fn(var=var, mean=mean, beta=beta, dim=dim, n_steps=neps)
+  # simple_scaled_context_integration_diffusion_multistep(stims=[])
+  # andre_drift(stims=[])
+  model_name += "_" + fn_name + "_" + stimset_name + "_swap-vars_"
+  model_name = model_name.replace(".", "")
+  param_string = 'm=%.2f, v=%.2f, b=%.2f, dim=%i, neps=%i' % (mean, var, beta, dim, neps)
+  print(param_string)
+  ## init task and net
+
+  # task sampling
+  # sample_task(pr_match,pr_slure,pr_clure)
+  if task == 'nback':
+    ssizeL = [2, 3] # [1, 3]
+    sample_fn = lambda match,slure,clure: lambda S,C,N: sample_nback_trial(S,C,N,
+                  pr_match=match,pr_stim_lure=slure,pr_context_lure=clure
+                  )
+  elif task == 'stern':
+    ssizeL = [4,9]
+    sample_fn = lambda match,slure,clure: lambda S,C,N: sample_sternberg_trial(S,C,N,
+                  pr_match=match,pr_stim_lure=slure,pr_context_lure=clure
+                  )
+
+
+
+  taskintL = [0,1]
+  taskL_tr = [
+    [taskintL[0],sample_fn(pr_match,pr_slure,pr_clure),ssizeL[0]],
+    [taskintL[1],sample_fn(pr_match,pr_slure,pr_clure),ssizeL[1]],
+  ]
+
+  # init net
+  net = FFWM(indim,hiddim)
+
+  ## train
+  # score_tr, ttype = run_model(net,taskL_tr,
+  #                     cfun = lambda idx, n_steps: long_walk(long_context, idx, n_steps),
+  #                     # cfun=lambda idx, n_steps: ctxt_fn(var=var, mean=mean, beta=beta, dim=dim, n_steps=n_steps)[0],
+  #                     stim_ntokens=stim_ntokens,
+  #                     sdim=sdim,
+  #                     n_context_steps=num_context_steps,
+  #                      training=True,
+  #                      neps_per_task=neps)
+
+  score_tr, ttype = train_model_intervals(net, taskL_tr,
+                              long_cfun=lambda idx, n_steps: long_walk(long_context, idx, n_steps),
+                              cfun=ctxt_fn,
+                              # cfun=lambda idx, n_steps: ctxt_fn(var=var, mean=mean, beta=beta, dim=dim, n_steps=n_steps)[0],
+                              stim_ntokens=stim_ntokens,
+                              sdim=sdim,
+                              n_context_steps=num_context_steps,
+                              training=True,
+                              neps_per_task=neps,
+                              model_name=model_name,
+                              stim_v=0.25)
+
+  np.save('2and3back/trsc-'+model_name,score_tr)
+  tr.save(net.state_dict(), '2and3back/trsc-'+model_name + ".pt")
+
+  ## eval
+  print('eval')
+  neps_ev = 1000
+  plot_train_acc(score_tr, ttype, fn_name, model_name, taskintL, ssizeL, net,
+                 sample_fn, neps_ev, ctxt_fn, long_context,
+                 stim_ntokens, sdim, num_context_steps)
+
+  ## plot train acc
+
+def plot_train_acc(score_tr, ttype, fn_name, model_name, taskintL, ssizeL, net,
+                 sample_fn, neps_ev, ctxt_fn, long_context,
+                 stim_ntokens, sdim, num_context_steps):
+  c = ["orange", "blue"]
+  m = ["o", "x", "v", "^"]
+
+  tasks = ["2-back", "3-back"]
+  colors = [["#edc174", "#d4982f", "#c27e08", "#a1690a"], ["#8dcbf7", "#61b3ed", "#2a86c7", "#0762a3"]]
+  labels = ["match", "slure", "clure", "nomatch"]
+  n_intervals = 1000
+  for i in range(len(score_tr)):
+    task_score = score_tr[i]
+    task_color = colors[i]
+    task_trialtypes = ttype[i]
+    for tt in range(4):
+      filt_inds = task_trialtypes == tt
+      ep_ttype = np.extract(filt_inds, task_score)
+      ep_ttype = ep_ttype[:-(len(ep_ttype)%n_intervals)]
+      ac = ep_ttype.reshape(-1, n_intervals).mean(1)
+      lt = tasks[i] + " " + labels[tt]
+      plt.plot(ac, color=task_color[tt], label=lt)
+  plt.legend(loc="best")
+  plt.ylim(0, 1)
+  plt.ylabel('Train accuracy, ' + fn_name)
+  plt.savefig('../Figures/2and3back/trac_%s'%(model_name))
+  plt.close('all')
+
+  # fig,axarr = plt.subplots(2,3,figsize=(20,8));axarr=axarr.reshape(-1)
+  # for nidx,noise_ev in enumerate([0,0.2,0.4,0.6,0.8,1.0]):
+  # ax = axarr[nidx]
+  for tidx, (task_int, ssize) in enumerate(zip(taskintL, ssizeL)):
+    plt.title('Accuracy by trial type, ' + fn_name)
+    evac = eval_byttype(net, sample_fn, ssize, task_int, neps_ev,
+                        long_context, stim_ntokens, sdim, num_context_steps)
+    np.save('exp-onehot_stim/evac-' + model_name + '-ssize_%i' % (ssize), evac)
+    plt.bar(np.arange(4) + (.45 * tidx), evac, width=.45)
+    # plt.ylim(0, 1)
+  plt.xticks(range(4), ['match', 'nomatch', 'slure', 'clure'])
+  plt.savefig('../Figures/2and3back/ttype_eval_%s' % (model_name))
+  plt.close('all')
+
+  sampling_dprime(net, model_name, ctxt_fn, mean, var, beta, dim, "../Figures/2and3back/")
+
+
+def eval_byttype(net, sample_fn, ssize, task_int, neps, long_context,
+                 stim_ntokens, sdim, num_context_steps):
+    """ eval on given task for separate trial types
+    returns evac on (match,nomatch,slure,clure)
+    """
+    taskL_ev = [
+      [task_int, sample_fn(1, 0, 0), ssize],
+      [task_int, sample_fn(0, 0, 0), ssize],
+      [task_int, sample_fn(0, 1, 0), ssize],
+      [task_int, sample_fn(0, 0, 1), ssize]
+    ]
+    evsc, ttype = run_model_for_epochs(
+      net, taskL_ev,
+      cfun=lambda idx, n_steps: long_walk(long_context, idx, n_steps),
+      # cfun=lambda idx, n_steps: ctxt_fn(var=var, mean=mean, beta=beta, dim=dim, n_steps=n_steps)[0],
+      stim_ntokens=stim_ntokens,
+      sdim=sdim,
+      n_context_steps=num_context_steps,
+      training=False,
+      neps_per_task=neps,
+      stim_v=0.25,
+      verb=False
+    )
+    evac = evsc.mean(1)
+    return evac
+
+
+
+
+sphere_drift = fast_n_sphere, [(0.25, 0.0, 0.0), (0.25, 0.05, 0.0), (0.25, 0.075, 0.0), (0.4, 0.075, 0.0)]
+
+functions = [sphere_drift]
+
+for seed in [1, 2]:
+  for function in functions:
+    fn = function[0]
+    params = function[1]
+    for dim in [25]:
+      for (mean, var, beta) in params:
+          run_all(seed=seed, ctxt_fn=fn, mean=mean, dim=dim, var=var, beta=beta, neps=400000)
